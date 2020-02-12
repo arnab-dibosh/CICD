8d8a33b58e8e217034f6e2b6262d726fbc362970
arnab_mohit <arnab_mohit>
2020-02-12 20:15:30 +0600

    8:15

--- AKS/Services/HelloNode/server.js
+++ AKS/Services/HelloNode/server.js
@@ -3,8 +3,8 @@ var http = require('http');
 var port = process.env.PORT || 80;
 
 http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'application/json' });
-    res.end('{ "message": "Hello World from cicd", "port": ' + port + ' }');
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.send('<h1>Hello World</h1>');
 }).listen(port, _cb => {
     console.log('started on port: ' + port);
 });
